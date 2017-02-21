848e338443b1da03c7a4bc921d77df952b9482a7
bibdectrl <bibliographicdecontrol@gmail.com>
2017-02-21 15:43:49 -0500

    done?

--- assignment.erl
+++ assignment.erl
@@ -1,7 +1,11 @@
 -module(assignment).
--export([perimeter/1, area/1, bits_direct/1, bits/1]).
+-export([perimeter/1, area/1, bits_direct/1, bits/1, enclose/1]).
 
 %Shape = {Name_as_atom, Dimensions}.
+
+
+%Define a function perimeter/1 which takes a shape and returns the perimeter of the shape.
+%Choose a suitable representation of triangles, and augment area/1 and perimeter/1 to handle this case too.
 
 perimeter({rectangle, {Width, Height}}) -> Width * 2 + Height * 2;
 perimeter({triangle, {A, B, C}}) -> A + B + C; 
@@ -14,30 +18,18 @@ area({rectangle, {Width, Height}}) ->
 area({circle, {Radius}}) ->
     math:pi() * Radius * Radius.
 
-
-enclose({triangle, {A, B, C}}) ->
-    ;
-enclose({rectangle, {Width, Height}) ->
-    ;
-enclose({circle, {Radius}}) ->
-    .
-
-%circle, rectangle, triangle
-
-
-
-%Shapes
-
-%Define a function perimeter/1 which takes a shape and returns the perimeter of the shape.
-
-%Choose a suitable representation of triangles, and augment area/1 and perimeter/1 to handle this case too.
-
 %Define a function enclose/1 that takes a shape an returns the smallest enclosing rectangle of the shape.
 
+enclose({triangle, {Base, Height, _}}) ->
+    {rectangle, {Base, Height}};
+enclose({rectangle, {Width, Height}}) ->
+    {rectangle, {Width, Height}};
+enclose({circle, {Radius}}) ->
+    {rectangle, {Radius*2, Radius*2}}.
 
-%Summing the bits
 
 %Define a function bits/1 that takes a positive integer N and returns the sum of the bits in the binary representation. For example bits(7) is 3 and bits(8) is 1.
+%See whether you can make both a direct recursive and a tail recursive definition.
 
 bits_direct(0) -> 0;
 bits_direct(N) when N rem 2 == 0 ->
@@ -51,8 +43,3 @@ bits_tail(0, Acc) ->
     Acc;
 bits_tail(N, Acc) -> bits_tail(N div 2, Acc + (N rem 2)).
 		   
-
-
-%See whether you can make both a direct recursive and a tail recursive definition.
-
-%Which do you think is better? Why?
